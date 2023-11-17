c4940ff3e6f53b409c996a0f75aa9b335c0a8390
shiozawa <a.shiozawa@newbees.jp>
2023-11-17 10:44:13 +0900

    Step1 修正 newbeescoltd/nb_study_react#3

--- booklog/src/App.tsx
+++ booklog/src/App.tsx
@@ -6,26 +6,35 @@ import { BookItem } from './types'
 function App() {
   const [bookItem, setBookItem] = useState<BookItem[]>(MOCK_DATA.items)
 
-  const Book = (bookInfo: BookItem): JSX.Element => {
+  const Book = ({
+    title,
+    authors,
+    description,
+    publisher,
+    imageLinks,
+    previewLink,
+  }: BookItem['volumeInfo']): JSX.Element => {
     return (
       <>
         <ul className="booklist">
-          <li className="listitem" key={bookInfo.id}>
-            {bookInfo.volumeInfo.imageLinks && (
+          <li className="listitem">
+            {imageLinks && (
               <div className="thumbnail">
-                <img src={bookInfo.volumeInfo.imageLinks.smallThumbnail} alt={bookInfo.volumeInfo.title} />
+                <img src={imageLinks.smallThumbnail} alt={title} />
               </div>
             )}
             <div className="rightblock">
-              <h2 className="title">{bookInfo.volumeInfo.title}</h2>
-              {bookInfo.volumeInfo.description && <p className="description">{bookInfo.volumeInfo.description}</p>}
-              <div className="sub-info">
-                {bookInfo.volumeInfo.authors && <p>著者: {bookInfo.volumeInfo.authors.join(`, `)}</p>}
-                {bookInfo.volumeInfo.publisher && <p>出版社: {bookInfo.volumeInfo.publisher}</p>}
-              </div>
-              {bookInfo.volumeInfo.previewLink && (
+              <h2 className="title">{title}</h2>
+              {description && <p className="description">{description}</p>}
+              {(authors || publisher) && (
+                <div className="sub-info">
+                  {authors && <p>著者: {authors.join(`, `)}</p>}
+                  {publisher && <p>出版社: {publisher}</p>}
+                </div>
+              )}
+              {previewLink && (
                 <div className="preview-link-box">
-                  <a className="preview-link" href={bookInfo.volumeInfo.previewLink} target="_blank">
+                  <a className="preview-link" href={previewLink} target="_blank">
                     詳しく見る
                   </a>
                 </div>
@@ -39,7 +48,7 @@ function App() {
 
   return (
     <>
-      <Book id={bookItem[0].id} volumeInfo={bookItem[0].volumeInfo} />
+      <Book {...bookItem[3].volumeInfo} />
     </>
   )
 }
