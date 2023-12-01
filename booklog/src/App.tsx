e1e62dd7c2fdf82f6289557845a25c092440e54a
shiozawa <a.shiozawa@newbees.jp>
2023-12-01 11:13:29 +0900

    step2 ブックリスト及び、ヘッダーを作成 newbeescoltd/nb_study_react#23

--- booklog/src/App.tsx
+++ booklog/src/App.tsx
@@ -1,54 +1,14 @@
 import './App.css'
-import { useState } from 'react'
-import { MOCK_DATA } from './mockdata'
-import { BookItem } from './types'
+import { Header } from './Header'
+import { Booklist } from './Booklist'
 
 function App() {
-  const [bookItem, setBookItem] = useState<BookItem[]>(MOCK_DATA.items)
-
-  const Book = ({
-    title,
-    authors,
-    description,
-    publisher,
-    imageLinks,
-    previewLink,
-  }: BookItem['volumeInfo']): JSX.Element => {
-    return (
-      <>
-        <ul className="booklist">
-          <li className="listitem">
-            {imageLinks && (
-              <div className="thumbnail">
-                <img src={imageLinks.smallThumbnail} alt={title} />
-              </div>
-            )}
-            <div className="rightblock">
-              <h2 className="title">{title}</h2>
-              {description && <p className="description">{description}</p>}
-              {(authors || publisher) && (
-                <div className="sub-info">
-                  {authors && <p>著者: {authors.join(`, `)}</p>}
-                  {publisher && <p>出版社: {publisher}</p>}
-                </div>
-              )}
-              {previewLink && (
-                <div className="preview-link-box">
-                  <a className="preview-link" href={previewLink} target="_blank">
-                    詳しく見る
-                  </a>
-                </div>
-              )}
-            </div>
-          </li>
-        </ul>
-      </>
-    )
-  }
-
   return (
     <>
-      <Book {...bookItem[3].volumeInfo} />
+      <Header />
+      <main className="main">
+        <Booklist />
+      </main>
     </>
   )
 }
