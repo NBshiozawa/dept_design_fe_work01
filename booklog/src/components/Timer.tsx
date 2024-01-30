e8a3cf975c4eb21199741edf811b6e9de9e75e46
shiozawa <a.shiozawa@newbees.jp>
2024-01-30 20:07:45 +0900

    step4 headerの読書時間タイマーを作成 newbeescoltd/nb_study_react#25

---
+++ booklog/src/components/Timer.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect, useRef } from 'react'
+import styles from './Timer.module.css'
+
+const times: number[] = [5, 10, 15]
+const zeroPadding = (time: number) => {
+  return String(time).padStart(2, '0')
+}
+
+export const Timer = () => {
+  const [countTime, setCountTime] = useState<number>(0)
+  const [isStart, setIsStart] = useState<boolean>(false)
+  const selectRef = useRef<HTMLSelectElement>(null)
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setCountTime(Number(selectRef.current?.value))
+    setIsStart(true)
+  }
+
+  useEffect(() => {
+    const countDown = setInterval(() => {
+      if (isStart && countTime > 0) {
+        setCountTime(countTime - 1)
+      }
+      if (countTime === 0) {
+        clearInterval(countDown)
+        setIsStart(false)
+      }
+    }, 1000)
+
+    return () => {
+      clearInterval(countDown)
+    }
+  }, [countTime])
+
+  return (
+    <>
+      <div className={styles.timer}>
+        <div className={styles.countblock}>
+          <p>読書タイマー</p>
+          <p className={styles.countnum}>00:00:{zeroPadding(countTime)}</p>
+        </div>
+        <form className={styles.timerform}>
+          <select className={styles.selectbox} ref={selectRef}>
+            {times.map((time) => (
+              <option value={time} key={time}>
+                {time}秒
+              </option>
+            ))}
+          </select>
+          <button type="submit" className={styles.button} onClick={handleClick}>
+            Start
+          </button>
+        </form>
+      </div>
+    </>
+  )
+}
