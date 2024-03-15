dd47b1f0b648fd5fb0de279258ad4b8fea618688
shio <a.shiozawa@newbees.jp>
2024-03-15 16:32:45 +0900

    step6 BookPreviewLinkの処理をご指摘いただいた内容に変更 newbeescoltd/nb_study_react#27

--- booklog/src/components/atoms/BookPreviewLink.tsx
+++ booklog/src/components/atoms/BookPreviewLink.tsx
@@ -4,14 +4,13 @@ type Props = {
   previewLink?: string
 }
 
-export const BookPreviewLink = ({ previewLink }: Props): JSX.Element => {
+export const BookPreviewLink = ({ previewLink }: Props): JSX.Element | null => {
+  if (typeof previewLink !== 'string') return null
+  // previewLinkがstring型でない場合はnullを返し、fragment(<></>)も返さない。
+  // previewLinkがstring型の場合は、a要素のみを返す。
   return (
-    <>
-      {previewLink === undefined ? null : (
-        <a className={styles.previewLink} href={previewLink} target="_blank">
-          詳しく見る
-        </a>
-      )}
-    </>
+    <a className={styles.previewLink} href={previewLink} target="_blank">
+      詳しく見る
+    </a>
   )
 }
